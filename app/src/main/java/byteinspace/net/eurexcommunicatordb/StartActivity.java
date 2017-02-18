0d98e69740948d94d6e69d61bbfe770825fbf1cd
Daniel Ludwig <contact@daniel-ludwig.net>
2017-02-18 13:56:08 +0100

    Switch so second screen

--- app/src/main/java/byteinspace/net/eurexcommunicatordb/StartActivity.java
+++ app/src/main/java/byteinspace/net/eurexcommunicatordb/StartActivity.java
@@ -1,8 +1,11 @@
 package byteinspace.net.eurexcommunicatordb;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.util.TimerTask;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -10,13 +13,29 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
-        try {
-            Thread.sleep(5000);
-        } catch (Exception e)
-        {
 
-        }
+        Handler handler = new Handler();
+        handler.postDelayed(getNewRunnable(), 5000);
+
+
+
+
+    }
+
+    protected void showNextScreen() {
         Intent intent = new Intent(this, IndexTicker.class);
         startActivity(intent);
+    }
+
+    private Runnable getNewRunnable() {
+        return  new Runnable() {
+
+            @Override
+            public void run() {
+                showNextScreen();
+
+
+            }
+        };
     }
 }
